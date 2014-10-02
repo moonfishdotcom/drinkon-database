121ffaeb6f86cc50252b7c1e8081625c9b972f5b
Tim Harris <tharris_hm@hotmail.com>
2014-10-02 12:59:37 +0100

    (#3) Revised tables and views, adding vendor_id

--- new/schema/tables/sys_customer_reviews.sql
+++ new/schema/tables/sys_customer_reviews.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS sys_customer_reviews;
 CREATE TABLE sys_customer_reviews
 (
   id int not null auto_increment,
+  vendor_id INT,
   customer_id INT,
   review_date DATETIME,
   review_text NVARCHAR(200),
--- new/schema/views/vw_customer_reviews.sql
+++ new/schema/views/vw_customer_reviews.sql
@@ -2,8 +2,9 @@ DROP VIEW IF EXISTS vw_customer_reviews;
 
 CREATE VIEW vw_customer_reviews AS
 SELECT
+  t1.vendor_id,
   t1.customer_id,
-  CONCAT_WS(' ', t2.customer_title, t2.customer_surname) AS customer_name,
-  t1.review_text,
+  CONCAT_WS(' ', t2.customer_title, t2.customer_surname, 'on', DATE_FORMAT(t1.review_date,'%W, %e, %M %Y'))  AS review_head,
+  t1.review_text AS review_body,
   t1.review_score
 FROM sys_customer_reviews t1 INNER JOIN vw_customers_with_titles t2 ON t1.customer_id=t2.id;
