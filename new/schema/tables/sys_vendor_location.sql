2231e0386f8cc4e6617d38a9632a4a3360aa3b8b
timharris123 <tharris_hm@hotmail.com>
2014-10-29 12:23:39 +0000

    (#42) Removed the dummy data lines

--- new/schema/tables/sys_vendor_location.sql
+++ new/schema/tables/sys_vendor_location.sql
@@ -8,7 +8,3 @@ CREATE TABLE sys_vendor_location
   location_lat DECIMAL(20,10),
   PRIMARY KEY (id)
 );
-
-
-INSERT INTO sys_vendor_location (vendor_id, location_long, location_lat) VALUES (1, 10.0011, 20.0022);
-INSERT INTO sys_vendor_location (vendor_id, location_long, location_lat) VALUES (2, 50.0011, 60.0022);
