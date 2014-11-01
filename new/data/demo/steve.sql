68fec9faa544589b265c8013e39267fab7c65641
Steve Patterson <spatte21@btinternet.com>
2014-11-01 10:04:11 +0000

    New stored proc to save an order

--- new/data/demo/steve.sql
+++ new/data/demo/steve.sql
@@ -37,8 +37,8 @@ VALUES
 
 INSERT INTO sys_order_statuses (id, order_status_name, is_active)
 VALUES
-(1, 'Accepted', TRUE),
-(2, 'Waiting', TRUE),
+(1, 'Waiting', TRUE),
+(2, 'Accepted', TRUE),
 (3, 'Complete', TRUE),
 (4, 'Pending', TRUE);
 
