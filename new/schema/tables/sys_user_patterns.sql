dd14ee0049bd0a5e0b2d0e3fec9924c72ebc6f19
timharris123 <tharris_hm@hotmail.com>
2014-11-08 10:22:46 +0000

    (#7) Add vendor chosen id to the patterns page

--- new/schema/tables/sys_user_patterns.sql
+++ new/schema/tables/sys_user_patterns.sql
@@ -4,6 +4,7 @@ CREATE TABLE sys_user_patterns
 (
   id int not null auto_increment,
   vendor_id INT,
+  vendor_pattern_id NVARCHAR(10),
   pattern_name NVARCHAR(100),
   is_active BIT,
   PRIMARY KEY (id)
--- new/schema/views/vw_vendor_patterns.sql
+++ new/schema/views/vw_vendor_patterns.sql
@@ -3,8 +3,9 @@ DROP VIEW IF EXISTS vw_vendor_patterns;
 CREATE VIEW vw_vendor_patterns AS 
 SELECT 
   id,
-  vendor_id, 
-  pattern_name, 
+  vendor_id,
+  vendor_pattern_id,
+  pattern_name,
   is_active
 FROM sys_user_patterns 
 ORDER BY id ASC;
